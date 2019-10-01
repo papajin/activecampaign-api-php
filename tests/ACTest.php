474d6d40e7d282210654d3ea973ee6c763d3aa26
papajin <papajini@gmail.com>
2019-10-01 10:26:38 +0300

    Add test of correct operation of the instance() method.

--- tests/ACTest.php
+++ tests/ACTest.php
@@ -21,4 +21,20 @@ class ACTest extends PHPUnit_Framework_TestCase {
 	public function testInstanceException() {
 		Contact::instance( 'https://account.api-us1.com' );
 	}
+
+	/**
+	 * @dataProvider instanceArgs
+	 */
+	public function testInstance( $http_client_or_url, $token = null ) {
+		$this->assertTrue(
+			Contact::instance( $http_client_or_url, $token ) instanceof Contact
+		);
+	}
+
+	public function instanceArgs() {
+		return [
+			['https://account.api-us1.com', 'ffjhgjg'],
+			[ new Client ]
+		];
+	}
 }
